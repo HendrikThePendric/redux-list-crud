40bc2c2df226795dd4e6cf6a85dead0fd9364a94
Hendrik de Graaf <hendrikdegraaf@gmail.com>
2018-06-09 20:51:58 +0200

    Indentation and  renamed lib to dist

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 export const dummy = (add: boolean) => {
-  const four = 4;
-  const six = 6;
-  const twelve = 12;
-  const twenty = 20;
-  if (add) {
-    return four + six + twelve + twenty;
-  }
-  return four - six;
+    const four = 4;
+    const six = 6;
+    if (add) {
+        return six + four;
+    }
+    return six - four;
 };
